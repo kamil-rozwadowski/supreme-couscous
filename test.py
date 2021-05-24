4f1d80fba0fef722c91fabc9cd655e5083b3a775
kazbiel <56132071+kazbiel@users.noreply.github.com>
2021-05-24 12:08:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print('hello word')
+
+print("pomyśl liczbe z zakresu 1 - 100")
+def referencja(p,k):
+    i = 0
+    s=int((p+k)/2)
+    print("czy twoja liczba to ",s," : ")
+    i=input()
+    if i =="tak":
+        print("yeeey")
+    else:
+        print("czy twoja liczba jest większa od ",s,"?")
+        a=input()
+        if a == "tak":
+            referencja(s+1,k)
+        else:
+            referencja(p,s - 1 )
+
+referencja(1,100)
