df6a478ffb10e24473863dd8ae1536ab67ec0086
Kazbiel <kakus0908@gmail.com>
2021-05-25 22:57:32 +0200

    Gra w mieszanie znaków w słowie

---
+++ mieszanie_znakow.py
@@ -0,0 +1,7 @@
+import random
+slowo=input("Podaj słowo: \n")
+dlugosc=len(slowo)
+dlugosc_minus=dlugosc*-1
+for i in range(0,dlugosc):
+    pozycja = random.randint(dlugosc_minus,dlugosc-1)
+    print(slowo[pozycja])
