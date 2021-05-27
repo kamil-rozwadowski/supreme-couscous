f38571d7b6cf79d414a509fa2c17ee0cd11894ba
Kazbiel <kakus0908@gmail.com>
2021-05-27 21:27:43 +0200

    Losowanie drużyn

---
+++ Randomizator.py
@@ -0,0 +1,25 @@
+import random
+
+a=[]
+licznik=1
+while 0<1:
+    x=input("Podaj imie uczestnika lub napisz 'stop': \n")
+    if x.lower()=="stop":
+        break
+    else:
+        a.append(x)
+
+teams= int(input("Podaj rozmiar dużyny: \n"))
+
+for i in a:
+
+    c = random.randrange(len(a))
+    print(a[c])
+    a = a[:c] + a[c + 1:]
+
+    if licznik == teams:
+        print("-----------------------------------------")
+        licznik = 0
+
+
+    licznik+=1
